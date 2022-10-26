75a3026bfbce7858697307ea961f0adaa8000745
ahmed <ahmedatigui001@gmail.com>
2022-10-27 00:44:33 +0100

    app page is Almost Done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Home() {
-
-  return(
-    <main>
+  return (
+    <main className="home">
       <nav>
-          <span className="logo">o;</span>
+        <span className="logo">o;</span>
       </nav>
       <section>
-          <h1>More features coming soon.</h1>
-          <p>This redesign is just the start of a series of new features and improvements for awwwards, here's a look at the evolution of the platform.</p>
-          <Link to="/app" className="cta">Locomotive</Link>
+        <h1>More features coming soon.</h1>
+        <p>
+          This redesign is just the start of a series of new features and
+          improvements for awwwards, here's a look at the evolution of the
+          platform.
+        </p>
+        <Link to="/app" className="cta">
+          Locomotive
+        </Link>
       </section>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
