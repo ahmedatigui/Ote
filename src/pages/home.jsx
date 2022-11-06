d8b9f8db18b88e99ed571d83edf8926fbacf3178
ahmed <ahmedatigui001@gmail.com>
2022-11-06 19:47:01 +0100

    Added a 3d model

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,31 +1,50 @@
 import { Link } from "react-router-dom";
 
+// 3D
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from "@react-three/drei"
+import Model from "../components/model.jsx"
+
 function Home() {
   return (
     <main className="home">
       <nav>
-        <span className="logo">o;</span>
+        <span className="logo">Ote</span>
       </nav>
       <section className="hero">
-        <h1>More features coming soon.</h1>
+        <h1>Modern online code editor.</h1>
         <p>
-          This redesign is just the start of a series of new features and
-          improvements for awwwards, here's a look at the evolution of the
-          platform.
+          This is a web-based online code editor, with support to three programming languages so far Cplusplus, Python, Javascript, and more to come soon.
         </p>
         <Link to="/app" className="cta">
-          Locomotive
+          Code
         </Link>
+        <div className="canvas-container">
+          <Canvas>
+            <directionalLight
+              castShadow
+              position={[2.5, 8, 5]}
+              shadow-mapSize={[1024, 1024]}
+              shadow-camera-far={50}
+              shadow-camera-left={-10}
+              shadow-camera-right={10}
+              shadow-camera-top={10}
+              shadow-camera-bottom={-10}
+            />
+            <pointLight color={"#e6eed6"} intensity={1.3} distance={100} />
+            <ambientLight color={"#b3ff00"} intensity={.4}/>
+            <Model rotation={[0, -Math.PI*.5, Math.PI*.05]} scale={[3, 3, 3]} position={[3.5, -4 , 0]}/>
+            <OrbitControls enableDamping enablePan enableRotate enableZoom />
+          </Canvas>
+        </div>
       </section>
       <section className="about">
         <div className="cards">
           <div className="card">
-            <div className="icon">D</div>
             <h3>Design</h3>
             <p>
-              This redesign is just the start of a series of new features and
-              improvements for awwwards, here's a look at the evolution of the
-              platform.
+              The current design is just the start of a series of improvements for Ote. Check the whole design roadmap by clicking the icon on the corner. 
             </p>
             <a
               href="https://www.figma.com/file/CEfXVHx3JnIfVRh5QWY6Jp/Online-text-editor-(Ote)?node-id=0%3A1"
@@ -45,32 +64,29 @@ function Home() {
                   height="19.5"
                   rx="4.75"
                   stroke="#ABACA7"
-                  stroke-width="0.5"
+                  strokeWidth="0.5"
                 />
                 <path
                   d="M6.74383 13.7415L12.9169 7.56848"
                   stroke="#565656"
-                  stroke-width="0.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="0.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M7.48976 7.33867L12.8213 7.26089C13.0441 7.25735 13.2244 7.43766 13.2209 7.6604L13.1431 12.992"
                   stroke="#565656"
-                  stroke-width="0.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="0.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </a>
           </div>
           <div className="card">
-            <div className="icon">D</div>
             <h3>Development</h3>
             <p>
-              This redesign is just the start of a series of new features and
-              improvements for awwwards, here's a look at the evolution of the
-              platform.
+              There were lots of decisions and creative solutions behind the development of Ote. Check the github repo through clicking the icon on the corner.
             </p>
             <a href="https://github.com/ahmedatigui/ote" target="_blank">
               <svg
@@ -87,21 +103,21 @@ function Home() {
                   height="19.5"
                   rx="4.75"
                   stroke="#ABACA7"
-                  stroke-width="0.5"
+                  strokeWidth="0.5"
                 />
                 <path
                   d="M6.74383 13.7415L12.9169 7.56848"
                   stroke="#565656"
-                  stroke-width="0.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="0.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M7.48976 7.33867L12.8213 7.26089C13.0441 7.25735 13.2244 7.43766 13.2209 7.6604L13.1431 12.992"
                   stroke="#565656"
-                  stroke-width="0.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="0.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </a>
