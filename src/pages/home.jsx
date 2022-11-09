500a7f87daf52756609f5a3dfe83dafc20fe647b
ahmed <ahmedatigui001@gmail.com>
2022-11-09 12:32:25 +0100

    removed 3d models

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,43 +1,21 @@
 import { Link } from "react-router-dom";
 
-// 3D
-import ReactDOM from 'react-dom'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from "@react-three/drei"
-import Model from "../components/model.jsx"
-
 function Home() {
   return (
     <main className="home">
-      <nav>
-        <span className="logo">Ote</span>
-      </nav>
-      <section className="hero">
-        <h1>Modern online code editor.</h1>
-        <p>
-          This is a web-based online code editor, with support to three programming languages so far Cplusplus, Python, Javascript, and more to come soon.
-        </p>
-        <Link to="/app" className="cta">
-          Code
-        </Link>
-        <div className="canvas-container">
-          <Canvas>
-            <directionalLight
-              castShadow
-              position={[2.5, 8, 5]}
-              shadow-mapSize={[1024, 1024]}
-              shadow-camera-far={50}
-              shadow-camera-left={-10}
-              shadow-camera-right={10}
-              shadow-camera-top={10}
-              shadow-camera-bottom={-10}
-            />
-            <pointLight color={"#e6eed6"} intensity={1.3} distance={100} />
-            <ambientLight color={"#b3ff00"} intensity={.4}/>
-            <Model rotation={[0, -Math.PI*.5, Math.PI*.05]} scale={[3, 3, 3]} position={[3.5, -4 , 0]}/>
-            <OrbitControls enableDamping enablePan enableRotate enableZoom />
-          </Canvas>
-        </div>
+      <section className="heropage">
+        <nav>
+          <span className="logo">Ote</span>
+        </nav>
+        <section className="hero">
+          <h1>Modern online code editor.</h1>
+          <p>
+            This is a web-based online code editor, with support to three programming languages so far Cplusplus, Python, Javascript, and more to come soon.
+          </p>
+          <Link to="/app" className="cta">
+            Code
+          </Link>
+        </section>
       </section>
       <section className="about">
         <div className="cards">
